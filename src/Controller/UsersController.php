fa5d12f05cf4b61a5528b4bf1a4e04d91cb7dc66
khinchanny <johnny@2ntkh.com>
2017-02-04 15:43:54 +0700

    add sth g

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -10,22 +10,7 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
-    public function login()
-    {
 
-        if($this->request->is('post')){
-            $user = $this->Auth->identify();
-            if($user){
-                $this->Auth->setUser($user);
-                return $this->redirect($this->Auth->redirectUrl());
-            } 
-            $this->Flash->errors(__('Invalid username or password, try again'));      
-        }
-
-    }
-     public function logout(){
-            return $this->redirect($this->Auth->logout());
-        }
 
     /**
      * Index method
@@ -131,4 +116,21 @@ class UsersController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
+
+        public function login()
+    {
+
+        if($this->request->is('post')){
+            $user = $this->Auth->identify();
+            if($user){
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            } 
+            $this->Flash->errors(__('Invalid username or password, try again'));      
+        }
+
+    }
+     public function logout(){
+            return $this->redirect($this->Auth->logout());
+        }
 }
